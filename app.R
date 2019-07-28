0f7bda56d3ed78073c54ec9cacafbd1bd8af54ba
Preeti31july <51479447+Preeti31july@users.noreply.github.com>
2019-07-28 21:35:13 +0530

    Add files via upload

---
+++ app.R
@@ -0,0 +1,17 @@
+
+
+#---------------------------------------------------------------------#
+#               Preeti Sharma   (11915023)                            #
+#               Kushagra Saxena (11915063)                            #
+#               Ujjwal Hadial   (11915031)                            #
+#---------------------------------------------------------------------#
+
+#---------------------------------------------------------------------#
+#               NLP in R with UDpipe                                  #
+#---------------------------------------------------------------------#
+
+library("shiny")
+
+source('https://github.com/Preeti31july/NLP---UDPipe-Shiny-App/blob/master/dependencycode-UDPipe-shinyApp.R')
+
+runGitHub('NLP---UDPipe-Shiny-App','Preeti31july')
